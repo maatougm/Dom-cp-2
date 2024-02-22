ebb51ba0a1d1026f7cd1286ac63dfa4c29910a0a
M.maatoug <maatougmoudafar@gmail.com>
2024-02-22 09:29:31 +0100

    cp dom 2

--- scipt.js
+++ scipt.js
@@ -7,10 +7,7 @@ function getRandomColor(){
 btn=document.getElementById("change-color-btn")
 box=document.getElementById("color-box")
 btn.addEventListener("click",function(){
-    var a =getRandomColor()
-    var b = getRandomColor()
-    var c = getRandomColor()
-    box.style.backgroundColor = "rgb(" + a + ", " + b + ", " + c + ")";
-   
+        
+    box.style.backgroundColor = `rgb(${getRandomColor()} , ${getRandomColor()} , ${getRandomColor()})`
 })
 
